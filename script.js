a3a909a8984bd3e43983f7542ce3ecbf6de76914
Gonthalo <gonzalogalpmg@gmail.com>
2016-07-05 17:03:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 
 acto1 = ducument.getElementById("acto1");
 acto2 = ducument.getElementById("acto2");
+audio = ducument.getElementById("audio");
 
 function show1(){
 	acto1.style.display = "block";
+	audio.src = "Faure4.mp3";
+	audio.load();
+	audio.play();
 }
 function show2(){
 	acto2.style.display = "block";
+	audio.pause()
+	audio.src = "Faure2.mp3";
+	audio.load();
+	audio.play();
 }
