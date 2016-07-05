998c3bdb7a2a601d25ec5b7278178e5565834eb5
Gonthalo <gonzalogalpmg@gmail.com>
2016-07-05 16:15:38 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+acto1 = ducument.getElementById("acto1")
+acto2 = ducument.getElementById("acto2")
+
+function show1(){
+	acto1.display = "block";
+}
+function show2(){
+	acto2.display = "block";
+}
