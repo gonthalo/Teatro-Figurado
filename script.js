5a1c52df674fcf2e37b813e30b7b10e94b595b6a
Gonthalo <gonzalogalpmg@gmail.com>
2016-07-05 16:29:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
-acto1 = ducument.getElementById("acto1")
-acto2 = ducument.getElementById("acto2")
+acto1 = ducument.getElementById("acto1");
+acto2 = ducument.getElementById("acto2");
 
 function show1(){
-	acto1.display = "block";
+	acto1.style.display = "block";
 }
 function show2(){
-	acto2.display = "block";
+	acto2.style.display = "block";
 }
